76dccf1735d856c03ef105da766cc05426c7d7f7
Jo Van Eyck <j.vaneyck@farmad.be>
2022-12-02 06:38:07 +0100

    template change: Lists instead of arrays by default

--- day 00/part1.fsx
+++ day 00/part1.fsx
@@ -1,11 +1,14 @@
 #r "nuget: Unquote"
 open Swensen.Unquote
 
-let input = System.IO.File.ReadAllLines $"""{__SOURCE_DIRECTORY__}\input.txt"""
+let input =
+    System.IO.File.ReadAllLines $"""{__SOURCE_DIRECTORY__}\input.txt"""
+    |> List.ofSeq
 
 let example =
     """hehe""".Split("\n")
     |> Array.map (fun s -> s.Trim())
+    |> List.ofSeq
 
 let run () =
     printf "Testing..."

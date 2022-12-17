2d275c3b78e2211c8c6b8690f1897fcf6701fe32
Jo Van Eyck <j.vaneyck@farmad.be>
2022-12-17 17:05:46 +0100

    17 naive simulation makes computer go boom for even the example

---
+++ day 17/part1.fsx
@@ -0,0 +1,171 @@
+#r "nuget: Unquote"
+open Swensen.Unquote
+
+let input = System.IO.File.ReadAllText $"""{__SOURCE_DIRECTORY__}\input.txt"""
+let example = """>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"""
+
+///A coordinate in our chamber.
+///       ymax
+///        |
+///        |
+/// (x,) x0,y0 __ __ xmax
+type Coord = int * int
+
+///A pattern is a set of coordinates relative to the shape's origin (0,0)
+type Pattern = Set<Coord>
+let pattern = set
+let contains c p = p |> Set.contains c
+
+///A rock keeps track of its location and its pattern
+type Rock = { Location: Coord; Pattern: Pattern }
+
+let absoluteCoords (rock: Rock) =
+    let ox, oy = rock.Location
+
+    rock.Pattern
+    |> Set.map (fun (x, y) -> (x + ox, y + oy))
+
+let print (pattern: Pattern) =
+    let xmin = pattern |> Seq.map fst |> Seq.min
+    let xmax = pattern |> Seq.map fst |> Seq.max
+    let ymin = pattern |> Seq.map snd |> Seq.min
+    let ymax = pattern |> Seq.map snd |> Seq.max
+
+    [ for y in ymax .. -1 .. ymin do
+          [ for x in xmin..xmax ->
+                if pattern |> contains (x, y) then
+                    printf "#"
+                else
+                    printf "." ]
+
+          printfn "" ]
+    |> ignore
+
+    printfn ""
+
+let patterns: Pattern seq =
+    [ pattern [ (0, 0)
+                (1, 0)
+                (2, 0)
+                (3, 0) ]
+      pattern [ (1, 0)
+                (1, 1)
+                (0, 1)
+                (2, 1)
+                (1, 2) ]
+      pattern [ (0, 0)
+                (1, 0)
+                (2, 0)
+                (2, 1)
+                (2, 2) ]
+      pattern [ (0, 0)
+                (0, 1)
+                (0, 2)
+                (0, 3) ]
+      pattern [ (0, 0)
+                (0, 1)
+                (1, 0)
+                (1, 1) ] ]
+
+patterns |> Seq.iter print
+
+let repeat s =
+    Seq.initInfinite (fun _ -> s) |> Seq.collect id
+
+///The camber contains all stable rocks
+type Chamber = { Width: int; Blocked: Set<Coord> }
+
+let emptyChamber width : Chamber =
+    let floor = [ for x in 0 .. (width - 1) -> (x, -1) ]
+    { Width = width; Blocked = set floor }
+
+let top chamber =
+    1 + (chamber.Blocked |> Seq.map snd |> Seq.max)
+
+let add (rock: Rock) (chamber: Chamber) =
+    { chamber with Blocked = Set.union chamber.Blocked (rock |> absoluteCoords) }
+
+let printChamber (chamber: Chamber) = print chamber.Blocked
+
+///A jet pushes left or right
+type Jet =
+    | Left
+    | Right
+
+let parse =
+    function
+    | '>' -> Right
+    | '<' -> Left
+    | u -> failwithf "Unknown jet: %c" u
+
+let apply (jet: Jet) (rock: Rock) : Rock =
+    let (x, y) = rock.Location
+
+    match jet with
+    | Left -> { rock with Location = (x - 1, y) }
+    | Right -> { rock with Location = (x + 1, y) }
+
+let collides (chamber: Chamber) (rock: Rock) : bool =
+    let coords = rock |> absoluteCoords
+
+    if coords |> Seq.exists (fun (x, y) -> x < 0) then
+        true
+    elif coords
+         |> Seq.exists (fun (x, y) -> x > (chamber.Width - 1)) then
+        true
+    elif Set.intersect chamber.Blocked coords
+         |> Seq.isEmpty
+         |> not then
+        true
+    else
+        false
+
+let push chamber jet rock =
+    let pushed = rock |> apply jet
+
+    if pushed |> collides chamber then
+        rock
+    else
+        pushed
+
+let lower height (rock: Rock) =
+    let (x, y) = rock.Location
+    { rock with Location = (x, y - height) }
+
+let drop (chamber: Chamber) (rock: Rock) : Rock =
+    let dropped = rock |> lower 1
+
+    if dropped |> collides chamber then
+        rock
+    else
+        dropped
+
+let rec falldown jets chamber rock =
+    let (j, js) = (Seq.head jets, Seq.tail jets)
+    let pushed = rock |> push chamber j
+    let dropped = pushed |> drop chamber
+
+    if dropped = pushed then
+        (js, pushed)
+    else
+        falldown js chamber dropped
+
+let fall (jets: Jet seq, chamber: Chamber) (pattern: Pattern) : (Jet seq * Chamber) =
+    let rock: Rock =
+        { Location = (2, 3 + (chamber |> top))
+          Pattern = pattern }
+
+    let js, dropped = falldown jets chamber rock
+    js, chamber |> add dropped
+
+let jets = example |> Seq.map parse |> repeat
+let rocks = patterns |> repeat |> Seq.take 2022
+let (_, c) = Seq.fold fall (jets, emptyChamber 7) rocks
+c |> printChamber
+
+let run () =
+    printf "Testing..."
+    test <@ 1 + 1 = 2 @>
+    printfn "...done!"
+
+run ()
